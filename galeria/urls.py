e838d05598a19fda3623ac4cdbeaa2a30806a71c
Romulo Cesar Silvestre Leite <Romulo34006947@edu.df.senac.br>
2025-03-27 21:37:57 -0300

    ok

--- galeria/urls.py
+++ galeria/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from galeria.views import index
+from galeria.views import imagem
 
 urlpatterns = [
-    path('', index)
+    path('', index,name='index'),
+    path('imagem/',imagem,name='imagem'),
 ]
