2703b2a275523b9cb505ab15e8ed12d1140a70bd
= <=>
2025-04-08 20:24:39 -0300

    ok

--- galeria/views.py
+++ galeria/views.py
@@ -1,11 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from galeria.models import Fotografia
 def index(request):
-    return render(request, 'galeria/index.html')
+    fotografia = Fotografia.objects.all()   
+    return render(request, 'galeria/index.html',{"cards":fotografia})
 
 def imagem(request):
     return render(request, 'galeria/imagem.html')
-
-
 
