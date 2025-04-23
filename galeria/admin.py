ac28e92c2f4e001b7fe052709716ad69cc75ac8f
= <=>
2025-04-23 19:51:30 -0300

    aula 22-04-2025

--- galeria/admin.py
+++ galeria/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+# todas as configuracoes ao django admin
+from galeria.models import Fotografia # importe a fotografia
+class ListandoFotografias(admin.ModelAdmin):
+    list_display = ("id","nome","legenda","descricao","foto")
+    list_display_links = ("nome","foto")
+    search_fields = ("nome",)
+
+admin.site.register(Fotografia,ListandoFotografias) #registre nosso banco de dados no admin
+# carregue agora.
